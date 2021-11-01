21c264868274a5aa08a6878fcf06d64569234a9d
Roombabatata <fares.setbels@gmail.com>
2021-11-01 17:54:37 +0100

    Add the function of open a file

--- display.py
+++ display.py
@@ -1,17 +1,16 @@
-from tkinter import filedialog, Tk, Button
+from tkinter import Label, filedialog, Tk, Button
 
 
-
+SUPPORTED_FILE_TYPES = ("Png Files", "*.png"), ("Jpg Files", "*.jpg"), ("All Files", "*.*")
 
 def choose_file():
-    file_chooser = filedialog.askopenfile(initialdir="~", title="Select an image", filetypes=(("Png Files", "*.png"), ("Jpg Files", "*.jpg"), ("All Files", "*.*")))
-
+    file_chooser = filedialog.askopenfile(initialdir="~", title="Select an image", filetypes=(SUPPORTED_FILE_TYPES))
+    return file_chooser
 root = Tk()
 root.title("Image Translator")
 # root.iconbitmap("assets/icons/icon.png")
 
-
-
 choose_file_button = Button(root, text="hello world", command=choose_file).pack()
+
 
 root.mainloop()
