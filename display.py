36831374d5db88c290bc7f4dffbbc0dee760676a
Roombabatata <fares.setbels@gmail.com>
2021-11-02 20:13:18 +0100

    fully inplemented the file choser

--- display.py
+++ display.py
@@ -5,12 +5,18 @@ SUPPORTED_FILE_TYPES = ("Png Files", "*.png"), ("Jpg Files", "*.jpg"), ("All Fil
 
 def choose_file():
     file_chooser = filedialog.askopenfile(initialdir="~", title="Select an image", filetypes=(SUPPORTED_FILE_TYPES))
-    return file_chooser
+    file_path = file_chooser.name
+    file_path_label = Label(root, text=f"Your document file: {file_path}")
+    file_path_label.pack()
+
+    return file_path
 root = Tk()
+
 root.title("Image Translator")
 # root.iconbitmap("assets/icons/icon.png")
 
 choose_file_button = Button(root, text="hello world", command=choose_file).pack()
 
+print(choose_file)
 
 root.mainloop()
